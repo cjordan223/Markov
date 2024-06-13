03185bc9dd3f6f44e68fe34f4d76ca8e8aa51e86
cjordan223 <126746175+cjordan223@users.noreply.github.com>
2024-06-12 22:18:21 -0700

    updated script

--- js/markov-generator.js
+++ js/markov-generator.js
@@ -1,44 +1,57 @@
-// js/markov-generator.js
-
 function buildMarkovChain(text) {
     const words = text.split(/\s+/);
     const chain = {};
 
-    for (let i = 0; i < words.length - 1; i++) {
-        const word = words[i];
-        const nextWord = words[i + 1];
+    for (let i = 0; i < words.length - 3; i++) {
+        const key = words.slice(i, i + 3).join(' ');
+        const nextWord = words[i + 3];
 
-        if (!chain[word]) {
-            chain[word] = [];
+        if (!chain[key]) {
+            chain[key] = [];
         }
 
-        chain[word].push(nextWord);
+        chain[key].push(nextWord);
     }
 
     return chain;
 }
 
-function generateText(chain, startWord, numWords = 50) {
-    let currentWord = startWord;
-    let result = [currentWord];
+function generateText(chain, numWords = 50) {
+    const keys = Object.keys(chain);
+    let currentKey = keys[Math.floor(Math.random() * keys.length)];
+    let result = currentKey.split(' ');
 
-    for (let i = 1; i < numWords; i++) {
-        const possibleNextWords = chain[currentWord];
+    for (let i = 3; i < numWords; i++) {
+        const possibleNextWords = chain[currentKey];
         if (!possibleNextWords || possibleNextWords.length === 0) break;
 
-        currentWord = possibleNextWords[Math.floor(Math.random() * possibleNextWords.length)];
-        result.push(currentWord);
+        const nextWord = possibleNextWords[Math.floor(Math.random() * possibleNextWords.length)];
+        result.push(nextWord);
+
+        currentKey = result.slice(result.length - 3).join(' ');
     }
 
-    return result.join(' ');
+    return postProcessText(result.join(' '));
+}
+
+function postProcessText(text) {
+    const sentences = text.split('. ').map(sentence => {
+        return sentence.charAt(0).toUpperCase() + sentence.slice(1);
+    });
+
+    let processedText = sentences.join('. ');
+
+    if (processedText.charAt(processedText.length - 1) !== '.') {
+        processedText += '.';
+    }
+
+    return processedText;
 }
 
 document.getElementById('generateBtn').addEventListener('click', function () {
     const inputText = document.getElementById('inputText').value;
-    const words = inputText.split(/\s+/);
-    const startWord = words[Math.floor(Math.random() * words.length)];
     const chain = buildMarkovChain(inputText);
-    const newText = generateText(chain, startWord);
+    const newText = generateText(chain);
 
     document.getElementById('output').innerText = newText;
 });
